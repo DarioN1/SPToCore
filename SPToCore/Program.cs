cc2dbeae52fd75ab9a1651eba1081566761d0d33
Dario Tordin <dario.tordin@live.com>
2020-05-11 15:19:59 +0200

    First Reliable Version

--- SPToCore/Program.cs
+++ SPToCore/Program.cs
@@ -5,6 +5,8 @@ using SPToCore.Model;
 using SPToCore.T4;
 using System;
 using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
@@ -13,6 +15,7 @@ using System.Linq;
 using System.Net.NetworkInformation;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
 //using SPToCore.T4;
 
 
@@ -23,7 +26,7 @@ using System.Text;
     TESTARE !!!
      
      */
-     
+
 [assembly: DesignTimeServicesReference(
     "EFCore.TextTemplating.DesignTimeServices, EFCore.TextTemplating")]
 
@@ -44,40 +47,15 @@ namespace SPToCore
         public static List<Sp> SpList = new List<Sp>();        
         public static List<SpException> ExceptionList = new List<SpException>();
 
-        static async System.Threading.Tasks.Task Main(string[] args)
-        {                       
-            //Console.Write(spToCoreT4Processed.TransformText());
+        static async Task<int> Main(string[] args)
+        {
+            var cmd = new RootCommand();            
+            cmd.AddCommand(SPToCoreScanCommand());
+            return await cmd.InvokeAsync(args);
+        }
 
-            //return;
-
-            //using (SPToCoreContext sp = new SPToCoreContext())
-            //{
-
-            //    //int prg = 10;
-            //    //int? idMat = 0;
-
-            //    //sp.Material_INITIALIZE(1, $"SPTOCORE{prg}", $"SPTOCORE MATERIAL {prg}", 1, 1, ref idMat);
-
-            //    //var res = await sp.Material_GETAsync(1, (int)idMat);
-
-            //    //foreach (var r in res)
-            //    //{
-            //    //    Console.WriteLine(r.Code);
-            //    //    Console.WriteLine(r.Material);
-            //    //}
-
-            //    var res2 = await sp.Material_SEARCHAsync(1, null, null, null);
-            //    foreach (var m in res2)
-            //    {
-            //        Console.WriteLine(m.Material);
-            //    }
-
-            //}
-
-            //return;
-
-
-
+        public static void SPToCoreScan() {
+            
             var dt_SpList = new DataTable();
             var dt_SpParam = new DataTable();
             var dt_SpResult = new DataTable();
@@ -92,55 +70,7 @@ namespace SPToCore
             int i = 1;
             string _schema = "";
             string _sp = "";
-
-            StringBuilder sResultDbSet = new StringBuilder();
-            StringBuilder sResultNoKey = new StringBuilder();
-            StringBuilder sResultMethods = new StringBuilder();
-            StringBuilder sResultClasses = new StringBuilder();
-
-            string template = $@"
-using System;
-using System.Data;
-using System.Collections.Generic;
-using Microsoft.Data.SqlClient;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using SPToCore.Test;  
-  
-namespace {P_NameSpace}.{P_OutPutSolutionFolder}
-{{
-public partial class SPToCoreContext : {P_ContextSource}
-    {{
-        [@DBSET]
-
-        public SPToCoreContext()
-        {{
-        }}
-
-        public SPToCoreContext(DbContextOptions<{P_ContextSource}> options)
-            : base(options)
-        {{
-        }}               
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {{
-
-            // No key
-            [@NOKEY]
-
-            //Thanks Valecass!!!
-            base.OnModelCreating(modelBuilder);
-        }}
-
-        
-        [@0]
-
-        [@1]
-        
-    }}
-}}
-                ";
-
+           
             foreach (DataRow r in dt_SpList.Rows) {                
                 
                 _schema = r["ROUTINE_SCHEMA"].ToString();
@@ -149,12 +79,7 @@ public partial class SPToCoreContext : {P_ContextSource}
                 dt_SpParam = Get_StoreProcedure_Param(_schema, _sp);
                 dt_SpResult = Get_StoreProcedure_Result(_schema, _sp);
 
-                Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH':'mm':'ss")} STEP 2 - {i} / {dt_SpList.Rows.Count} ==> \"{r["ROUTINE_NAME"]}\" (Parameters: {dt_SpParam.Rows.Count}, OutputColumns: {dt_SpResult.Rows.Count}");
-
-                sResultDbSet.AppendLine(SPToCore_GenerateDbSet(_sp, _schema, dt_SpParam, dt_SpResult));
-                sResultNoKey.AppendLine(SPToCore_GenerateNoKey(_sp, _schema, dt_SpParam, dt_SpResult));
-                sResultMethods.AppendLine(SPToCore_GenerateMethod(_sp,_schema, dt_SpParam, dt_SpResult));
-                sResultClasses.AppendLine(SPToCore_GenerateResult(_sp,_schema, dt_SpResult));                
+                Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH':'mm':'ss")} STEP 2 - {i} / {dt_SpList.Rows.Count} ==> \"{r["ROUTINE_NAME"]}\"");
 
                 var pList = new List<SpParam>();
                 foreach (DataRow par in dt_SpParam.Rows) {
@@ -226,274 +151,133 @@ public partial class SPToCoreContext : {P_ContextSource}
                 SpList.Add(sp);
 
                 i++;
-            }
-
-            template = template.Replace("[@DBSET]", sResultDbSet.ToString());
-            template = template.Replace("[@NOKEY]", sResultNoKey.ToString());
-            template = template.Replace("[@0]", sResultMethods.ToString());
-            template = template.Replace("[@1]", sResultClasses.ToString());
-            
-            File.WriteAllText(Path.Combine(P_OutPutPhysicalFolder, "SPToCoreContext.cs"), template);
+            }                        
 
             Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH':'mm':'ss")} FINISH");
-
             
             SPToCoreT4 spToCoreT4Processed = new SPToCoreT4(SpList, P_ContextSource);
 
             File.WriteAllText(Path.Combine(P_OutPutPhysicalFolder, "SPToCoreContextT4.cs"), spToCoreT4Processed.TransformText());
 
             if (ExceptionList.Count > 0) {
-                Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH':'mm':'ss")} EXCEPTION FOUND! Please check SPToCore_log.txt");
+                Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH':'mm':'ss")} EXCEPTION FOUND! Please check SPToCore_log.txt in '{P_OutPutPhysicalFolder}'");
                 WriteException();
             }
             
-            
         }
 
-        private static string SPToCore_GenerateDbSet(string _sp, string _schema, DataTable _dtParam, DataTable _dtResult)
+
+        private static Command SPToCoreScanCommand()
         {
-            if (_dtResult.Rows.Count == 0)
-                return "";
-
-            if (_dtResult.Select("name <> ''").Length == 0)
-                return "";
-
-            return $@"private DbSet<{_sp}Result> {_sp} {{ get; set; }}";
-        }
-
-        private static string SPToCore_GenerateNoKey(string _sp, string _schema, DataTable _dtParam, DataTable _dtResult)
-        {
-            if (_dtResult.Rows.Count == 0)
-                return "";
-
-            if (_dtResult.Select("name <> ''").Length == 0)
-                return "";
-
-            return $@"modelBuilder.Query<{_sp}Result>().HasNoKey();";
-        }
-
-        private static string SPToCore_GenerateMethod(string _sp,string _schema, DataTable _dtParam, DataTable _dtResult)
-        {            
-            string template = $@"                    
-                        public [@H0] ([@H1])  
-                        {{
-                            [@B0]
-
-                            try{{
-                                [@B1]
-
-                                [@B2]
-
-                                [@B3]
-                                [@B4]
-                            }}
-                            catch(Exception ex){{
-                                throw(ex);
-                            }}
-
-                            [@F0]
-                        }}  
-            ";
-
-            StringBuilder sbH1 = new StringBuilder();
-            StringBuilder sbB1 = new StringBuilder();
-
-            string methodReturnType;
-            string outputResult;
-            string name;
-            string type;
-            bool isNullable;
-            bool isOutput;
-            int i;
-
-            //[@H0] ReturnType Definition
-            if (_dtResult.Rows.Count == 0)
+            var cmd = new Command("scan", "Show a greating to a specific person!!!");
+            cmd.AddOption(new Option(new[] { "--connection", "-cnn" }, "SQL SERVER 2012+ Connection String")
             {
-                //if has no result, its a void ( like crud operations )
-                methodReturnType = $"void {_sp}";             
-            }
-            else
-            {
-                methodReturnType = $"async Task<List<{_sp}Result>> {_sp}Async";                
-            }
-
-            //[@B0] Result declaration
-            if (_dtResult.Rows.Count == 0)
-            {
-                outputResult = "";
-            }
-            else
-            {                
-                outputResult = $@"// Initialize Result.  
-            List<{_sp}Result> lst = new List<{_sp}Result>();";                
-            }
-
-            //[@H1] method Parameters
-            foreach (DataRow r in _dtParam.Rows)
-            {
-                name = r["Parameter"].ToString().Replace("@", "");
-                type = r["Type"].ToString();
-                isNullable = (bool)r["is_nullable"];
-                isOutput = (bool)r["is_Output"];
-
-                sbH1.Append($"{(isOutput ? "ref " : "")}{SP_GetType(type, isNullable)} {name}, ");                
-            }
-            string methodParameters = sbH1.ToString();
-            if (methodParameters.Length > 0)
-                methodParameters = methodParameters.Remove(methodParameters.Length - 2, 2);
-
-            //[@B1] SP Parameters
-            if (_dtParam.Rows.Count > 0) { 
-                foreach (DataRow r in _dtParam.Rows)
+                Argument = new Argument<string>()
                 {
-                    name = r["Parameter"].ToString().Replace("@", "");
-                    type = r["Type"].ToString();
-                    isNullable = (bool)r["is_nullable"];
-                    isOutput = (bool)r["is_Output"];
-
-                    sbB1.AppendLine($@"SqlParameter p_{name} = new SqlParameter(""@{name}"", {name} ?? (object)DBNull.Value);");
+                    Arity = ArgumentArity.ExactlyOne
                 }
             }
-
-            //[@B2] SP CALL
-            string processing = $@"// Processing.  
-                string sqlQuery = $@""EXEC [{_schema}].[{_sp}] {Environment.NewLine} [@PARAMS]"";";
-
-            StringBuilder sbSpParam = new StringBuilder();
-            foreach (DataRow r in _dtParam.Rows)
+            );
+            cmd.AddOption(new Option(new[] { "--schema", "-sch" }, "SQL SERVER Schema (set * for no filter)")
             {
-                name = r["Parameter"].ToString().Replace("@", "");
-                type = r["Type"].ToString();
-                isNullable = (bool)r["is_nullable"];
-                isOutput = (bool)r["is_Output"];
-
-                sbSpParam.AppendLine($"@{name} {(isOutput ? " OUTPUT" : "")}, ");                
-            }
-            string p = "";
-            if (sbSpParam.Length > 0)
-                p = ReplaceLastOccurrence(sbSpParam.ToString(), ", ", "");
-
-            processing = processing.Replace("[@PARAMS]", p);
-
-            //[@B3] OutputParams
-            string output3 = "";
-            string output4 = "";
-            i = 0;
-
-            StringBuilder sbOutParam = new StringBuilder();
-            if(_dtResult.Rows.Count > 0){
-                output3 = $"lst = await this.{_sp}.FromSqlRaw(sqlQuery[@PARAMS]).ToListAsync();";
-
-                StringBuilder sbOutP = new StringBuilder();
-                if (_dtParam.Rows.Count > 0)
+                Argument = new Argument<string>()
                 {
-                    foreach (DataRow r in _dtParam.Rows)
-                    {
-                        name = r["Parameter"].ToString().Replace("@", "");
-                        type = r["Type"].ToString();
-                        isNullable = (bool)r["is_nullable"];
-                        isOutput = (bool)r["is_Output"];
+                    Arity = ArgumentArity.ExactlyOne
+                }
+            }
+            );
+            cmd.AddOption(new Option(new[] { "--namespace", "-nsp" }, "Solution namespace")
+            {
+                Argument = new Argument<string>()
+                {
+                    Arity = ArgumentArity.ExactlyOne
+                }
+            }
+            );
+            cmd.AddOption(new Option(new[] { "--context", "-ctx" }, "Context from")
+            {
+                Argument = new Argument<string>()
+                {
+                    Arity = ArgumentArity.ExactlyOne
+                }
+            }
+            );
+            cmd.AddOption(new Option(new[] { "--sfolder", "-sf" }, "Solution Folder (ex 'Model')")
+            {
+                Argument = new Argument<string>()
+                {
+                    Arity = ArgumentArity.ExactlyOne
+                }
+            }
+            );
+            cmd.AddOption(new Option(new[] { "--pfolder", "-pf" }, @"Physical Destination Folder (ex 'C:\MySolution\Model')")
+            {
+                Argument = new Argument<string>()
+                {
+                    Arity = ArgumentArity.ExactlyOne
+                }
+            }
+            );
+            cmd.Handler = CommandHandler.Create<string,string,string,string,string,string >((cnn, nsp,sch, ctx,sf, pf) => {
+                P_ConnectionString = cnn;      
+                P_NameSpace = nsp;
+                P_Schema = sch;
+                P_ContextSource = ctx;
+                P_OutPutSolutionFolder = sf;
+                P_OutPutPhysicalFolder = pf;
 
-                        sbOutP.Append($"p_{name}, ");
+                
+
+                if (!String.IsNullOrEmpty(cnn) &&
+                   !String.IsNullOrEmpty(sch) &&
+                   !String.IsNullOrEmpty(nsp) &&
+                   !String.IsNullOrEmpty(ctx) &&
+                   !String.IsNullOrEmpty(sf) &&
+                   !String.IsNullOrEmpty(pf)
+                )
+                {
+                    Console.WriteLine(P_ConnectionString);
+                    Console.WriteLine(P_NameSpace);
+                    Console.WriteLine(P_Schema);
+                    Console.WriteLine(P_ContextSource);
+                    Console.WriteLine(P_OutPutSolutionFolder);
+                    Console.WriteLine(P_OutPutPhysicalFolder);
+
+                    SPToCoreScan();
+
+                }
+                else {
+                    Console.WriteLine("SPToCore - ERROR:");
+                    if (String.IsNullOrEmpty(cnn))
+                    {
+                        Console.WriteLine("Parameter Missing: connection");
                     }
-                    string tmp = sbOutP.ToString();
-                    if (tmp.Length > 0)
-                        tmp = tmp.Remove(tmp.Length - 2, 2);
-
-                    output3 = output3.Replace($"[@PARAMS]", $", {tmp}");
-                }
-                else
-                {
-                    output3 = output3.Replace("[@PARAMS]", "");
-                }
-            }
-            if (_dtParam.Rows.Count > 0)
-            {
-                foreach (DataRow r in _dtParam.Rows)
-                {
-                    name = r["Parameter"].ToString().Replace("@", "");
-                    type = r["Type"].ToString();
-                    isNullable = (bool)r["is_nullable"];
-                    isOutput = (bool)r["is_Output"];
-
-                    if (isOutput)
+                    if (String.IsNullOrEmpty(sch))
                     {
-                        sbOutParam.AppendLine($"{name} = ({SP_GetType(type, isNullable)})p_{name}.Value;");
-                        
-                        i++;
+                        Console.WriteLine("Parameter Missing: schema");
+                    }
+                    if (String.IsNullOrEmpty(nsp))
+                    {
+                        Console.WriteLine("Parameter Missing: namespace");
+                    }
+                    if (String.IsNullOrEmpty(ctx))
+                    {
+                        Console.WriteLine("Parameter Missing: context");
+                    }
+                    if (String.IsNullOrEmpty(sf))
+                    {
+                        Console.WriteLine("Parameter Missing: solution folder");
+                    }
+                    if (String.IsNullOrEmpty(pf))
+                    {
+                        Console.WriteLine("Parameter Missing: physical folder");
                     }
                 }
-                output4 = sbOutParam.ToString();
+
             }
-            
-
-            //[@F0] Return List
-            string retList = "";
-            if (_dtResult.Rows.Count > 0)
-            {
-                retList = $"return lst;";
-            }
-
-
-            template = template.Replace("[@H0]", methodReturnType);
-            template = template.Replace("[@H1]", methodParameters);
-
-            template = template.Replace("[@B0]", outputResult);
-            template = template.Replace("[@B1]", sbB1.ToString());
-            template = template.Replace("[@B2]", processing);
-            template = template.Replace("[@B3]", output3);
-            template = template.Replace("[@B4]", output4);
-
-            template = template.Replace("[@F0]", retList);
-
-
-
-            return template;
+            );
+            return cmd;
         }
-
-        private static string SPToCore_GenerateResult(string _sp,string _schema, DataTable _dtResult)
-        {
-            if (_dtResult.Rows.Count == 0)
-                return "";
-
-            if (_dtResult.Select("name <> ''").Length == 0)
-                return "";
-
-            if (_sp == "Material_GET") {
-                int i = 0;
-            }
-            
-
-            string template = $@"                    
-                        public class {_sp}Result  
-                        {{
-                            [@0]
-                        }}  
-            ";
-
-            StringBuilder sb = new StringBuilder();
-
-            string name = "";
-            string type = "";
-            bool isNullable;
-
-            foreach (DataRow r in _dtResult.Rows) {
-
-                name = r["name"].ToString();
-                if (name != "")
-                {
-                    type = r["system_type_name"].ToString();
-                    isNullable = (bool)r["is_nullable"];
-
-                    sb.AppendLine($"public {SP_GetType(type, isNullable)} {name} {{get; set;}}");
-                }
-            }
-
-            return template.Replace("[@0]", sb.ToString());            
-        }
-
-        
-
 
         private static string SP_GetType(string type,bool isNullable)
         {
